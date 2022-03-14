f9804f760862d6baa53fa16e4efa7c49b6c97569
minaLMG <beandro6@gmail.com>
2022-03-14 23:11:49 +0200

    used UseContext hook instead of the contect consumer

--- src/App.js
+++ src/App.js
@@ -33,12 +33,13 @@ function App() {
       <AuthContext.Provider
         value={{
           isLoggedIn: isLoggedIn,
+          onLogout: logoutHandler,
         }}
       >
-        <MainHeader onLogout={logoutHandler} />
+        <MainHeader />
         <main>
           {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
+          {isLoggedIn && <Home />}
         </main>
       </AuthContext.Provider>
     </React.Fragment>
